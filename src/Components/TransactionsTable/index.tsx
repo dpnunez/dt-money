6543463fd0d6d0d5f3879988f8821bde2ffb7991
Daniel Nunez <daniel.nunez@nave.rs>
2021-07-13 21:11:00 -0300

    feat: add miragejs (api mock)

--- src/Components/TransactionsTable/index.tsx
+++ src/Components/TransactionsTable/index.tsx
@@ -1,35 +1,44 @@
-import { formatMoney } from "Helpers"
+import { useEffect } from "react"
 import { Container } from "./styles"
+import { formatMoney } from "Helpers"
+import { fetchClient } from "Services/fetchClient"
 
-const TransactionsTable = () => (
-	<Container>
-		<table>
-			<thead>
-				<tr>
-					<th>Título</th>
-					<th>Valor</th>
-					<th>Categoria</th>
-					<th>Data</th>
-				</tr>
-			</thead>
+const TransactionsTable = () => {
 
-			<tbody>
-				<tr>
-					<td>Lorem ipsum</td>
-					<td>{formatMoney(200)}</td>				
-					<td>Ipsum</td>
-					<td>20</td>
-				</tr>
-				<tr>
-					<td>Lorem ipsum</td>
-					<td className="">{formatMoney(230)}</td>				
-					<td>Ipsum</td>
-					<td>20</td>
-				</tr>
-			</tbody>
-		</table>
-	</Container>
-)
+	useEffect(() => {
+		fetchClient.get('/transactions').then(response => console.log(response)).catch(e => console.log(e))
+	}, [])
 
+	return (
+		<Container>
+			<table>
+				<thead>
+					<tr>
+						<th>Título</th>
+						<th>Valor</th>
+						<th>Categoria</th>
+						<th>Data</th>
+					</tr>
+				</thead>
+	
+				<tbody>
+					<tr>
+						<td>Lorem ipsum</td>
+						<td>{formatMoney(200)}</td>				
+						<td>Ipsum</td>
+						<td>20</td>
+					</tr>
+					<tr>
+						<td>Lorem ipsum</td>
+						<td className="">{formatMoney(230)}</td>				
+						<td>Ipsum</td>
+						<td>20</td>
+					</tr>
+				</tbody>
+			</table>
+		</Container>
+	)
+	
+}
 
 export { TransactionsTable }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+import { createServer } from 'miragejs'
+
+createServer({
+	routes() { 
+		this.namespace = 'api';
+		
+		this.get('/transactions', () => ([
+			{
+				id: 1,
+				title: 'Lorem',
+				amount: 123,
+				type: 'deposit',
+				category: 'Food',
+				createdAt: new Date()
+			}
+		]))
+	}
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
