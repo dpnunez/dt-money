6d65977fe6a9aa7b8ad9759b5070e8e44977c6e2
Daniel Nunez <daniel.nunez@nav9.tech>
2023-01-10 15:05:50 -0300

    feat: add summary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,39 +1,12 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { SummaryCard, SummaryContainer } from './styles'
 import { useTheme } from 'styled-components'
-import { useContext } from 'react'
-import { TransactionsContext } from '../../contexts/TransactionsContext'
 import { priceFormatter } from '../../utils/formatters'
-
-interface SummaryResponse {
-  income: number
-  outcome: number
-  total: number
-}
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
   const theme = useTheme()
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary: SummaryResponse = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
-
+  const summary = useSummary()
   console.log('render')
 
   return (
