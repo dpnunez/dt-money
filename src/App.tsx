f904d2fa7935b1736da8163fcc3bcad267b0fcdc
Daniel Nunez <daniel.nunez@nav9.tech>
2023-01-10 13:11:29 -0300

    feat: add header components (layout)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes'
 import { GlobalStyles } from './styles/global'
+import { Transactions } from './pages/transactions'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>Hello</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header'
+
+const Transactions = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export { Transactions }
